64d360bef809c4c1bb8306e791689b6db53c27b0
Steve Hegyes <stephen.hegyes@outlook.com>
2022-09-03 16:15:36 -0500

    week 9 assignment

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cf/b01b9cf1b82b001d1665af3bbd79a47f
@@ -0,0 +1,16 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DbConnection.getConnection();
+		
+		
+		
+	}
+
+}
---
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,19 @@
+package projects.service;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+	
+	private ProjectDao projectDao = new ProjectDao();
+	
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+	
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+	
+
+}
